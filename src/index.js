fee9048c321cb3dd19dddd2d76f6b700819755b8
wingedrasengan927 <ms.neerajkrishna@gmail.com>
2026-07-02 19:51:44 +0530

    update packaging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 // src/index.js
+import "./editor_style.css";
+
 export { default as initializeEditor } from "./editor.js";
 export { default as defaultEditorTheme } from "./editorTheme.js";
 
-import "./editor_styles.css";
+// Export custom transformers
+export { HR_TRANSFORMER } from "./transformers/HRTransformer.js";
+export { IMAGE_TRANSFORMER } from "./transformers/ImageTransformer.js";
+export {
+	MATH_INLINE_TRANSFORMER,
+	MATH_BLOCK_SINGLE_LINE_TRANSFORMER,
+	MATH_BLOCK_MULTILINE_TRANSFORMER,
+	MATH_HIGHLIGHT_BLOCK_TRANSFORMER,
+} from "./transformers/MathTransformer.js";
